1cbb073ea469f5be7989e106d41015bfe9223928
Alex Marshall <alex.k.marshall83@gmail.com>
2021-11-05 15:14:11 +0000

    update hsl colors

--- src/styles/color.css.ts
+++ src/styles/color.css.ts
@@ -2,27 +2,27 @@ import { createGlobalTheme } from '@vanilla-extract/css'
 
 export const colorThemeTokens = createGlobalTheme(':root', {
   background: {
-    body: 'hsl(185, 41%, 84%)',
-    card: 'hsl(0, 0%, 100%)',
-    input: 'hsl(203, 50%, 97%)',
-    accent: 'hsl(183, 100%, 15%)',
-    accentActive: 'hsl(172, 67%, 45%)',
-    accentHover: 'hsl(173, 61%, 77%)',
-    accentDisabled: 'hsl(183, 79%, 24%)',
+    body: 'hsl(185 41% 84%)',
+    card: 'hsl(0 0% 100%)',
+    input: 'hsl(203 50% 97%)',
+    accent: 'hsl(183 100% 15%)',
+    accentActive: 'hsl(172 67% 45%)',
+    accentHover: 'hsl(173 61% 77%)',
+    accentDisabled: 'hsl(183 79% 24%)',
   },
   text: {
     light: {
-      neutral: 'hsl(183, 100%, 15%)',
-      secondary: 'hsl(186, 14%, 43%)',
+      neutral: 'hsl(183 100% 15%)',
+      secondary: 'hsl(186 14% 43%)',
     },
     dark: {
-      neutral: 'hsl(0, 0%, 100%)',
-      secondary: 'hsl(184, 14%, 56%)',
+      neutral: 'hsl(0 0% 100% / 0.95)',
+      secondary: 'hsl(184 14% 56%)',
     },
   },
   tone: {
-    brand: 'hsl(172, 67%, 45%)',
-    critical: 'hsl(15, 50%, 57%)',
+    brand: 'hsl(172 67% 45%)',
+    critical: 'hsl(15 50% 57%)',
   },
 })
 
